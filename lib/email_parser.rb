7ef0a65d3c2dda55e7f5e049b775f58cfb0d6daf
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-07 17:19:15 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,6 +4,10 @@
 # or whitespace (' ').
 
 class EmailAddressParser
-  attr_accessor :name, :csv_emails
-  def initialize(csv_emails)
+  attr_accessor :name, :emails
+  
+  def initialize(emails)
+    @emails = emails
+  end
+  
 end 
